1c4e0bbdda42c3fda39d8142358c7874ccb3b84e
Sergio <sergylozanodelrio@gmail.com>
2020-10-02 10:51:56 +0200

    first commit

---
+++ src/renderer.jsx
@@ -0,0 +1,7 @@
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log('Loaded React.');
+
+ReactDOM.render(<div>Test.</div>, document.getElementById('root'));
